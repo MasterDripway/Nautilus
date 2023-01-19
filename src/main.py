91b4b7c6277bd8136edc937d7188704cc1b21d42
MasterDripway <manuelinegron@gmail.com>
2023-01-19 16:17:19 -0400

    changed main and gitignore

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from lib.install import install
 def main():
     print("[+] Starting installation")
     fetch('https://github.com/MasterDripway/Nautilus.git')
-    install("/home/manuel/proj/nautilus/src/Nautilus")
+    install("~/proj/Nautilus/src/Nautilus")
 
 if __name__ == '__main__':
     main()

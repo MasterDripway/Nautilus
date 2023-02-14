6d25ccd1aa7ff3c915c51a3fca4a58444868438c
= <manuelinegron@gmail.com>
2023-02-14 16:22:40 -0400

    .:wq

--- installer.py
+++ installer.py
@@ -1,4 +1,9 @@
 from lib.install import install
-
+import os
 
 install("setup")
+cwd = os.getcwd()
+HOME = os.getenv("HOME", "/home/")
+print('[+] Adding NAUTILUS_HOME to ~/.bashrc')
+with open(HOME + '/.bashrc', 'a') as writer:
+    writer.write(f"export NAUTILUS_HOME=\"{cwd}\"")
